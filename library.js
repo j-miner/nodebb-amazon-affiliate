b5fde782fc16708559d9001855569747498fa481
j-miner <134248113+j-miner@users.noreply.github.com>
2023-10-05 22:29:35 -0700

    Update library.js

--- library.js
+++ library.js
@@ -1,116 +1,51 @@
-'use strict';
+(function(module) {
+    "use strict";
 
-const nconf = require.main.require('nconf');
-const winston = require.main.require('winston');
+    // domain used to attempt to isolate errors to just the plugin
+    var domain = require('domain'),
+        d = domain.create();
 
-const meta = require.main.require('./src/meta');
+    d.on('error', function(err) {
+        console.error('nodebb-plugin-amazon-affiliate error: ' + err);
+    });
 
-const controllers = require('./lib/controllers');
+    d.run(function () {
+        var url = module.parent.require('url'), // https://nodejs.org/api/url.html
+            Entities = module.parent.require('html-entities').Html5Entities, // https://www.npmjs.com/package/html-entities
+            entities = new Entities(),
+            AmazonAffiliate = {},
+            affiliateCode = 'oppositelock-20';
 
-const routeHelpers = require.main.require('./src/routes/helpers');
+        module.exports = AmazonAffiliate;
 
-const plugin = {};
+        // common replacement code
+        AmazonAffiliate.replaceAmazonLinks = function(fromText) {
+            var link_rewrite = function (match, p1, offset, search_string) {
+                    var parsed_matched_url = url.parse(entities.decode(p1), true, true),
+                        match1_len = p1.length,
+                        p1_match_offset = match.indexOf(p1);
+                    parsed_matched_url.query['tag']= affiliateCode;
+                    parsed_matched_url.search = undefined; // required to get "url" module to use the "query" dict instead of the "search" string
 
-plugin.init = async (params) => {
-	const { router /* , middleware , controllers */ } = params;
+                    // glue the new html entitized URL inside matchdaviwort05
+                    return match.substring(0, p1_match_offset) + entities.encode(url.format(parsed_matched_url)) + match.substring(p1_match_offset + match1_len);
+                };
 
-	// Settings saved in the plugin settings can be retrieved via settings methods
-	const { setting1, setting2 } = await meta.settings.get('quickstart');
-	if (setting1) {
-		console.log(setting2);
-	}
+            return fromText.replace(/<a href="((?:https?:\/\/)?(?:www\.)?amazon\.com\/[^"]*)".*>.+<\/a>/ig, link_rewrite);
+        };
 
-	/**
-	 * We create two routes for every view. One API call, and the actual route itself.
-	 * Use the `setupPageRoute` helper and NodeBB will take care of everything for you.
-	 *
-	 * Other helpers include `setupAdminPageRoute` and `setupAPIRoute`
-	 * */
-	routeHelpers.setupPageRoute(router, '/quickstart', [(req, res, next) => {
-		winston.info(`[plugins/quickstart] In middleware. This argument can be either a single middleware or an array of middlewares`);
-		setImmediate(next);
-	}], (req, res) => {
-		winston.info(`[plugins/quickstart] Navigated to ${nconf.get('relative_path')}/quickstart`);
-		res.render('quickstart', { uid: req.uid });
-	});
+        // for posts
+        AmazonAffiliate.parse = function(data, callback) {
+            if (data.hasOwnProperty('postData')) {
+                data.postData.content = AmazonAffiliate.replaceAmazonLinks(data.postData.content);
+            } else if (data.hasOwnProperty('userData') && data.userData.hasOwnProperty('signature')) {
+                data.userData.signature = AmazonAffiliate.replaceAmazonLinks(data.userData.signature);
+            } else if (data.hasOwnProperty('parsedMessage')) {
+                data.parsedMessage = AmazonAffiliate.replaceAmazonLinks(data.parsedMessage);
+            }
 
-	routeHelpers.setupAdminPageRoute(router, '/admin/plugins/quickstart', [], controllers.renderAdminPage);
-};
+            callback(null, data);
+        };
+    });
 
-/**
- * If you wish to add routes to NodeBB's RESTful API, listen to the `static:api.routes` hook.
- * Define your routes similarly to above, and allow core to handle the response via the
- * built-in helpers.formatApiResponse() method.
- *
- * In this example route, the `ensureLoggedIn` middleware is added, which means a valid login
- * session or bearer token (which you can create via ACP > Settings > API Access) needs to be
- * passed in.
- *
- * To call this example route:
- *   curl -X GET \
- * 		http://example.org/api/v3/plugins/quickstart/test \
- * 		-H "Authorization: Bearer some_valid_bearer_token"
- *
- * Will yield the following response JSON:
- * 	{
- *		"status": {
- *			"code": "ok",
- *			"message": "OK"
- *		},
- *		"response": {
- *			"foobar": "test"
- *		}
- *	}
- */
-plugin.addRoutes = async ({ router, middleware, helpers }) => {
-	const middlewares = [
-		middleware.ensureLoggedIn,			// use this if you want only registered users to call this route
-		// middleware.admin.checkPrivileges,	// use this to restrict the route to administrators
-	];
-
-	routeHelpers.setupApiRoute(router, 'get', '/quickstart/:param1', middlewares, (req, res) => {
-		helpers.formatApiResponse(200, res, {
-			foobar: req.params.param1,
-		});
-	});
-};
-
-plugin.addAdminNavigation = (header) => {
-	header.plugins.push({
-		route: '/plugins/quickstart',
-		icon: 'fa-tint',
-		name: 'Quickstart',
-	});
-
-	return header;
-};
-const AmazonPlugin = {
-name: "Amazon Affiliate Plugin",
-description: "This plugin looks for an Amazon link in every post and turns it into an Amazon affiliate link.",
-init() {
-// Get the user's Amazon affiliate code.
-const affiliateCode = this.config.get("oppositelock-20");
-
-// Create a function that takes an Amazon link and returns an Amazon affiliate link.
-const getAffiliateLink = (link) => {
-  // Remove the "tag=" parameter from the link, if it exists.
-  const noTagLink = link.replace(/tag=[^&]*/, "");
-
-  // Add the affiliate code to the link.
-  return noTagLink + "&tag=" + affiliateCode;
-};
-
-// Listen for post events and replace any Amazon links with affiliate links.
-this.on("post", (post) => {
-  // Find all the Amazon links in the post.
-  const amazonLinks = post.content.match(/https?:\/\/www\.amazon\.com\/([\w-]+)/g);
-
-  // Replace the Amazon links with affiliate links.
-  for (const amazonLink of amazonLinks) {
-    post.content = post.content.replace(amazonLink, getAffiliateLink(amazonLink));
-  }
-});
-},
-};
-
-module.exports = AmazonPlugin;
+}(module));
