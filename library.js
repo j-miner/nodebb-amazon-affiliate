f9705662433bd326c37d6b499d46cf0b165a8d26
j-miner <134248113+j-miner@users.noreply.github.com>
2023-10-06 21:46:21 -0700

    Update library.js

--- library.js
+++ library.js
@@ -1,25 +1,21 @@
-const { Plugin } = require('nodebb-plugin');
+const nodebb = require('nodebb');
 
-module.exports = class AmazonAffiliateLinksPlugin extends Plugin {
-  constructor() {
-    super();
+const plugin = nodebb.plugin();
 
-    this.affiliateTag = 'YOUR_AFFILIATE_TAG'; // Replace this with your Amazon affiliate tag
-  }
+plugin.init = function() {
+  // Register a post filter to convert Amazon links to affiliate links.
+  nodebb.filters.register('prePostRender', function(post) {
+    // Find all Amazon links in the post content.
+    const amazonLinks = post.content.match(/https:\/\/www\.amazon\.(com|co\.uk|de|fr|es|it|jp|ca|cn|in)\/dp\/([A-Z0-9]+)/g);
 
-  async init() {
-    const hook = 'filter:post.content';
-
-    this.app.filter(hook, async (content) => {
-      // Find all Amazon links in the post content
-      const amazonLinks = content.match(/https?:\/\/([a-z]+\.)?amazon\.com\/[a-zA-Z0-9]+/g);
-
-      // Convert each Amazon link to an affiliate link
-      amazonLinks.forEach((link) => {
-        content = content.replace(link, `${link}?tag=${this.affiliateTag}`);
-      });
-
-      return content;
+    // Convert each Amazon link to an affiliate link.
+    amazonLinks.forEach(amazonLink => {
+      const affiliateLink = `https://affiliate-link.com/${amazonLink.split('/')[3]}`;
+      post.content = post.content.replace(amazonLink, affiliateLink);
     });
-  }
-}
+
+    return post;
+  });
+};
+
+module.exports = plugin;
