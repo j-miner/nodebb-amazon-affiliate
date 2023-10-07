4534b21ca127e008262e3a5900391527c1a3c235
j-miner <134248113+j-miner@users.noreply.github.com>
2023-10-06 22:02:56 -0700

    Update library.js

--- library.js
+++ library.js
@@ -1,25 +1,23 @@
-const { Plugin } = require('nodebb-plugin');
+// nodebb-plugin-amazon-affiliate
+// Converts Amazon links to affiliate links in posts
 
-module.exports = class AmazonAffiliateLinksPlugin extends Plugin {
-  constructor() {
-    super();
+const plugin = {
+  name: 'Amazon Affiliate',
+  version: '1.0.0',
+  description: 'Converts Amazon links to affiliate links in posts.',
+  init: async function() {
+    // Get the Amazon affiliate tag
+    const affiliateTag = await this.config('amazon.affiliateTag');
 
-    this.affiliateTag = 'oppo-20'; // Replace this with your Amazon affiliate tag
-  }
+    // Register a hook to convert Amazon links in posts
+    this.hooks.register('render:post', async function(post) {
+      // Regex to match Amazon links
+      const amazonLinkRegex = /(https?:\/\/[a-z]+\.amazon\.com\/[^\s]+)/g;
 
-  async init() {
-    const hook = 'filter:post.content';
-
-    this.app.filter(hook, async (content) => {
-      // Find all Amazon links in the post content
-      const amazonLinks = content.match(/https?:\/\/([a-z]+\.)?amazon\.com\/[a-zA-Z0-9]+/g);
-
-      // Convert each Amazon link to an affiliate link
-      amazonLinks.forEach((link) => {
-        content = content.replace(link, `${link}?tag=${this.affiliateTag}`);
-      });
-
-      return content;
+      // Replace all Amazon links in the post with affiliate links
+      post.content = post.content.replace(amazonLinkRegex, `https://www.amazon.com/${affiliateTag}/$1`);
     });
   }
-}
+};
+
+module.exports = plugin;
